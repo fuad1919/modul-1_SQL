6b439b8de14bb9c20b43b821d882f85939be24ac
fuad1919 <149462644+fuad1919@users.noreply.github.com>
2024-05-07 17:24:47 +0700

    Add files via upload

---
+++ modul satu.sql
@@ -0,0 +1,23 @@
+-- Nama : Husnul fuad 
+-- Nim  :22241066
+-- prodi :pendidikan teknologi
+
+-- mengunakan database undikma_mart
+use undikma_mart
+
+-- mengambil 1 data nama _produk dari table ms_produk
+select namaa_produk, harga FROM ms_produk_dqlab;
+ 
+ -- mengambil lebih dari satu kolom 
+ select nama_produk, harga from ms_produk_dqlab;
+ 
+ -- mengambil seluruh kolom table
+ select * from ms_produk_dqlab;
+ 
+ -- latihan 
+ -- 1. mengambil kolom kode_produk, nama_produk dari table ms_produk
+ select kode_produk, nama_produk from ms_produk_dqlab;
+ 
+ -- 2. mengambil seluruh kolom dari table tr_penjualan
+ select * from tr_
+ 
